00d1724f55e66458686b40a56ab8dfd85be7b5db
Fabian Canas <fcanas@gmail.com>
2014-12-14 15:21:55 -0500

    Refactor composite filters to a common foundation function

--- SwiftImage/CompositeFilters.swift
+++ SwiftImage/CompositeFilters.swift
@@ -8,267 +8,109 @@
 
 import Foundation
 
-func sourceOverCompositing(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CISourceOverCompositing", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
+public typealias Overlay = CIImage -> Filter
+
+func overlayWithName(filterName: String) -> Overlay {
+    return {overlay in
+        return { image in
+            let parameters : CIParameters = [
+                kCIInputBackgroundImageKey: image,
+                kCIInputImageKey: overlay
+            ]
+            let filter = CIFilter(name:filterName, withInputParameters: parameters)
+            return filter.outputImage
+        }
     }
 }
 
 func additionCompositing(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIAdditionCompositing", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIAdditionCompositing")(overlay)
 }
 
 func colorBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIColorBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIColorBlendMode")(overlay)
 }
 
 func colorBurnBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIColorBurnBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIColorBurnBlendMode")(overlay)
 }
 
 func colorDodgeBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIColorDodgeBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIColorDodgeBlendMode")(overlay)
 }
 
 func darkenBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIDarkenBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIDarkenBlendMode")(overlay)
 }
 
 func differenceBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIDifferenceBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIDifferenceBlendMode")(overlay)
 }
 
 func exclusionBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIExclusionBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIExclusionBlendMode")(overlay)
 }
 
 func hardLightBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIHardLightBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIHardLightBlendMode")(overlay)
 }
 
 func hueBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIHueBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIHueBlendMode")(overlay)
 }
 
 func lightenBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CILightenBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CILightenBlendMode")(overlay)
 }
 
 func luminosityBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CILuminosityBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CILuminosityBlendMode")(overlay)
 }
 
 func maximumCompositing(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIMaximumCompositing", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIMaximumCompositing")(overlay)
 }
 
 func minimumCompositing(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIMinimumCompositing", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIMinimumCompositing")(overlay)
 }
 
 func multiplyBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIMultiplyBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIMultiplyBlendMode")(overlay)
 }
 
 func multiplyCompositing(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIMultiplyCompositing", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIMultiplyCompositing")(overlay)
 }
 
 func overlayBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIOverlayBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIOverlayBlendMode")(overlay)
 }
 
 func saturationBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CISaturationBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CISaturationBlendMode")(overlay)
 }
 
 func screenBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CIScreenBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CIScreenBlendMode")(overlay)
 }
 
 func softLightBlendMode(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CISoftLightBlendMode", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CISoftLightBlendMode")(overlay)
 }
 
 func sourceAtopCompositing(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CISourceAtopCompositing", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CISourceAtopCompositing")(overlay)
 }
 
 func sourceInCompositing(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CISourceInCompositing", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CISourceInCompositing")(overlay)
 }
 
 func sourceOutCompositing(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CISourceOutCompositing", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CISourceOutCompositing")(overlay)
 }
 
 func sourceOverCompositing(overlay: CIImage) -> Filter {
-    return { image in
-        let parameters : CIParameters = [
-            kCIInputBackgroundImageKey: image,
-            kCIInputImageKey: overlay
-        ]
-        let filter = CIFilter(name:"CISourceOverCompositing", withInputParameters: parameters)
-        return filter.outputImage.imageByCroppingToRect(image.extent())
-    }
+    return overlayWithName("CISourceOverCompositing")(overlay)
 }
-
