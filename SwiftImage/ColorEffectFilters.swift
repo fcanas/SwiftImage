144b6e3084667c31d4f470c419b97ad6a493c406
Fabian Canas <fcanas@gmail.com>
2014-12-14 14:06:30 -0500

    Add all remaining CICategoryColorEffect category filters

--- SwiftImage/ColorEffectFilters.swift
+++ SwiftImage/ColorEffectFilters.swift
@@ -52,3 +52,169 @@ func colorInvert() -> Filter {
         return filter.outputImage
     }
 }
+
+func colorMap(gradientImage: CIImage) -> Filter {
+    return { image in
+        let parameters : CIParameters = [
+            "inputGradientImage":gradientImage,
+            kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIColorMap", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func colorMonochrome(color: CIColor, intensity: Float) -> Filter {
+    return { image in
+        let parameters : CIParameters = [
+            kCIInputColorKey:color,
+            kCIInputIntensityKey:intensity,
+            kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIColorMonochrome", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func colorPosterize(levels: Float) -> Filter {
+    return { image in
+        let parameters : CIParameters = [
+            "inputLevels":levels,
+            kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIColorPosterize", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func falseColor(color0: CIColor, color1: CIColor) -> Filter {
+    return { image in
+        let parameters : CIParameters = [
+            "inputColor0":color0,
+            "inputColor1":color1,
+            kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIFalseColor", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func maskToAlpha() -> Filter {
+    return { image in
+        let parameters : CIParameters = [kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIMaskToAlpha", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func maximumComponent() -> Filter {
+    return { image in
+        let parameters : CIParameters = [kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIMaximumComponent", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func minimumComponent() -> Filter {
+    return { image in
+        let parameters : CIParameters = [kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIMinimumComponent", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func photoEffectChrome() -> Filter {
+    return { image in
+        let parameters : CIParameters = [kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIPhotoEffectChrome", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func photoEffectFade() -> Filter {
+    return { image in
+        let parameters : CIParameters = [kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIPhotoEffectFade", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func photoEffectInstant() -> Filter {
+    return { image in
+        let parameters : CIParameters = [kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIPhotoEffectInstant", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func photoEffectMono() -> Filter {
+    return { image in
+        let parameters : CIParameters = [kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIPhotoEffectMono", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func photoEffectNoir() -> Filter {
+    return { image in
+        let parameters : CIParameters = [kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIPhotoEffectNoir", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func photoEffectProcess() -> Filter {
+    return { image in
+        let parameters : CIParameters = [kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIPhotoEffectProcess", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func photoEffectTonal() -> Filter {
+    return { image in
+        let parameters : CIParameters = [kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIPhotoEffectTonal", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func photoEffectTransfer() -> Filter {
+    return { image in
+        let parameters : CIParameters = [kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIPhotoEffectTransfer", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func sepiaTone(intensity: Float) -> Filter {
+    return { image in
+        let parameters : CIParameters = [
+            kCIInputIntensityKey:intensity,
+            kCIInputImageKey: image]
+        let filter = CIFilter(name:"CISepiaTone", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func vignette(radius: Float, intensity: Float) -> Filter {
+    return { image in
+        let parameters : CIParameters = [
+            kCIInputRadiusKey:radius,
+            kCIInputIntensityKey:intensity,
+            kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIVignette", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+func vignetteEffect(center: CGPoint, radius: Float, intensity: Float) -> Filter {
+    return { image in
+        let parameters : CIParameters = [
+            kCIInputCenterKey:CIVector(CGPoint:center),
+            kCIInputRadiusKey:radius,
+            kCIInputIntensityKey:intensity,
+            kCIInputImageKey: image]
+        let filter = CIFilter(name:"CIVignette", withInputParameters:parameters)
+        return filter.outputImage
+    }
+}
+
+
+
