a29aabaf536b303fb3e24dc9c333e4ddbc85b33a
Fabian Canas <fcanas@gmail.com>
2014-12-14 23:44:56 -0500

    Add CICategorySharpen filters

---
+++ SwiftImage/SharpenFilters.swift
@@ -0,0 +1,30 @@
+//
+//  SharpenFilters.swift
+//  SwiftImage
+//
+//  Created by Fabian Canas on 12/14/14.
+//  Copyright (c) 2014 Fabián Cañas. All rights reserved.
+//
+
+import Foundation
+
+func sharpenLuminance(sharpness: Float) -> Filter {
+    return { image in
+        let parameters = [
+            kCIInputImageKey:image,
+            kCIInputSharpnessKey:sharpness
+        ]
+        return CIFilter(name:"CISharpenLuminance", withInputParameters:parameters).outputImage
+    }
+}
+
+func unsharpMask(radius: Float, intensity: Float) -> Filter {
+    return { image in
+        let parameters = [
+            kCIInputImageKey:image,
+            kCIInputRadiusKey:radius,
+            kCIInputIntensityKey:intensity,
+        ]
+        return CIFilter(name:"CISharpenLuminance", withInputParameters:parameters).outputImage
+    }
+}
