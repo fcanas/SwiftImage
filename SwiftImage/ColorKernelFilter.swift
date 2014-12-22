7aa56eaa3a7481b22d25a0592130722144700474
Fabian Canas <fcanas@gmail.com>
2014-12-21 20:09:59 -0500

    Add basic Color Kernel filter without parameters

---
+++ SwiftImage/ColorKernelFilter.swift
@@ -0,0 +1,41 @@
+//
+//  ColorKernelFilter.swift
+//  SwiftImage
+//
+//  Created by Fabian Canas on 12/21/14.
+//  Copyright (c) 2014 Fabián Cañas. All rights reserved.
+//
+
+import CoreImage
+
+public class ColorKernelFilter: CIFilter {
+    var kernelString :String
+    lazy var kernel :CIColorKernel = {
+        return CIColorKernel(string: self.kernelString)
+        }()
+    
+    var inputImage :CIImage?
+    
+    init(kernel: String) {
+        kernelString = kernel
+        super.init()
+    }
+    
+    required public init(coder aDecoder: NSCoder) {
+        kernelString = ""
+        super.init(coder: aDecoder)
+        if let k = aDecoder.decodeObjectForKey("SwiftImageKernelStringKey") as? String {
+            kernelString = k
+        }
+    }
+    
+    override public func encodeWithCoder(aCoder: NSCoder) {
+        super.encodeWithCoder(aCoder)
+        aCoder.encodeObject(kernelString, forKey: "SwiftImageKernelStringKey")
+    }
+    
+    func outputImage() -> CIImage? {
+        return kernel.applyWithExtent(inputImage!.extent(), arguments: [inputImage!])
+    }
+}
+
